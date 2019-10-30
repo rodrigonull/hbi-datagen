e10d06e0c1ee27cff8d72b259605bf2302856971
Brandon Tweed <btweed@redhat.com>
2019-10-29 20:03:11 -0400

    Fix the name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="hbi-datagen-catastrophe-brandon",
+    name="hbi-datagen",
     version="0.0.1",
     author="Brandon Tweed",
     author_email="zbrandontweed@gmail.com",
